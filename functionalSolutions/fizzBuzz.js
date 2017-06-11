30b45068dd09ad6a7b6f7d5b39ef6e90bb9197ff
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-06-11 16:35:18 -0700

    Create fizzBuzz.js

--- functionalSolutions/fizzBuzz.js
+++ functionalSolutions/fizzBuzz.js
@@ -7,6 +7,7 @@ For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 */
 
+//EXAMPLE #1
 let amt = 101;
 let ray = Array(amt);
 ray.fill(0,0)
@@ -15,5 +16,12 @@ ray.fill(0,0)
              ind % 3 === 0 ? (ind % 5 === 0 ? "fizzBuzz" : "fizz")
              : ind : ind;
 });
-    
-    
+console.log(ray);
+
+
+//EXAMPLE #2
+var play = [...Array(100).keys()];
+play = play.map(i => i + 1)
+  .map(i => i % 15 == 0 ? "FizzBuzz" : i)
+  .map(i => i % 3  == 0 ? "Fizz" : i)
+  .map(i => i % 5  == 0 ? "Buzz" : i)
