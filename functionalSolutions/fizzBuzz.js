0bfe56455f68f396562a2cef1d9f3e234901d1cb
Cyborg_Ean <E-A-N@users.noreply.github.com>
2017-06-11 16:36:18 -0700

    Create fizzBuzz.js

--- functionalSolutions/fizzBuzz.js
+++ functionalSolutions/fizzBuzz.js
@@ -21,7 +21,7 @@ console.log(ray);
 
 //EXAMPLE #2
 var rayray = [...Array(100).keys()];
-rayray = play.map(i => i + 1)
+rayray = rayray.map(i => i + 1)
   .map(i => i % 15 == 0 ? "FizzBuzz" : i)
   .map(i => i % 3  == 0 ? "Fizz" : i)
   .map(i => i % 5  == 0 ? "Buzz" : i)
